d021ae47f63091ae7ba6464b27fe24b14df3a8ba
gstravinskaite <g.stravinskaite@elsevier.com>
2022-08-19 17:58:19 +0200

    adjusted JSON

--- food.py
+++ food.py
@@ -6,7 +6,7 @@ import json
 
 def run(args):
     
-    meals = retrieval.give_meals(data, args.meals)
+    meals = retrieval.give_meals(DATA, args.meals)
     for meal in meals:
         print(f"{meal.index}: {meal.name}")
         if args.ingredients:
@@ -16,7 +16,7 @@ def run(args):
 
 def run_grocery_list(gl):
 
-    grocery_dict = grocery_list.merge_produce_lists(data, gl)
+    grocery_dict = grocery_list.merge_produce_lists(DATA, gl)
     for ingredient, quantity in grocery_dict.items():
         print("\t--{}: {}".format(ingredient, quantity)) 
 
@@ -34,7 +34,7 @@ if __name__ == "__main__":
                         help="The number of meals to generate")
     parser.add_argument("-i", "--interactive", default=False, type=bool,
                         metavar="I", help="Should prompt be interactive")
-    parser.add_argument("-f", "--file", type=str, default="test.json",
+    parser.add_argument("-f", "--file", type=str, default="meals.json",
                         help="Path to the meals file")
     parser.add_argument("-g", "--ingredients", action="store_true",
                         help="Show ingredients")
@@ -45,7 +45,7 @@ if __name__ == "__main__":
     validate(args)
 
     with open(args.file) as f:
-        data = json.load(f)
+        DATA = json.load(f)
 
     if args.grocerylist:
         run_grocery_list(args.grocerylist)
